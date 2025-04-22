d8f5d5b988bb54ca0ceeb2a68ba26405c4aab538
laksh-krishna-sharma <asklakshsharma@gmail.com>
2025-04-22 20:31:17 +0530

    minor check

--- backend/app/modules/db.py
+++ backend/app/modules/db.py
@@ -2,7 +2,7 @@ import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 
-load_dotenv()  # Load environment variables from .env
+load_dotenv()
 
 DB_URI = os.getenv("DB_URI")
 if not DB_URI:
@@ -10,8 +10,7 @@ if not DB_URI:
 
 # Create an async client and get a reference to your database.
 client = AsyncIOMotorClient(DB_URI)
-db = client.get_default_database()  # Uses the database specified in the URI, or you can specify one: client["my_database"]
+db = client.get_default_database()  
 
-# Optionally, you can create helper functions to return collections:
 def get_collection(collection_name: str):
     return db[collection_name]
